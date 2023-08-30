aa64f5f40ef28b99bb0f14b5892864836b75ba3a
prabhakararunasalam <142766340+prabhakararunasalam@users.noreply.github.com>
2023-08-31 01:39:03 +0530

    Add files via upload
    question no 3

--- hosting.js
+++ hosting.js
@@ -1,17 +1,28 @@
 //https://restcountries.com/v3.1/all
 let xhr=new XMLHttpRequest();
 console.log(xhr);
+//geting data from the api
 
 xhr.open("GET","https://restcountries.com/v3.1/all");
+//send request
 xhr.send();
+//load the api
+//and apply the function and get the data what we need
 xhr.onload= function() {
     const Data = JSON.parse(xhr.response);
+    console.log(xhr.response);
     console.log(Data);
     console.log(xhr.status);
     for(i=0; i<Data.length; i++){
-        console.log("Name :", Data[i].name.common);
-        console.log("flag :", Data[i].flags.png);
-    }
+        console.log(`
+        Name : ${Data[i].name.common}
+        flag : ${Data[i].flags.png}
+        region : ${Data[i].region}
+        subregion : ${Data[i].subregion}
+        population : ${Data[i].population}
+        `)
+    
+        }
       
 
 
